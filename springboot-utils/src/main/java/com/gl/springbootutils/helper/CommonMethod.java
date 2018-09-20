afb512ab6a4572cc466573d48590b95aabbbcd2a
shuidichuanshi <43435319+shuidichuanshi@users.noreply.github.com>
2018-09-20 16:32:50 +0800

    Add files via upload

---
+++ springboot-utils/src/main/java/com/gl/springbootutils/helper/CommonMethod.java
@@ -0,0 +1,13 @@
+package com.gl.springbootutils.helper;
+
+/**
+ * //TODO 添加类/接口功能描述
+ *
+ * @author zb
+ * @date 2018-09-20
+ */
+public class CommonMethod {
+    public void f(){
+        System.out.println("1232131");
+    }
+}
